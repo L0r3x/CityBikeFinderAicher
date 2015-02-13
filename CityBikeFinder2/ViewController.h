6e03b5f7c15a2352e457e2fd48142b5a727df8bc
Michael Aicher <mia@foryouandyourcustomers.com>
2015-02-13 22:35:30 +0100

    First local Commit

---
+++ CityBikeFinder2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityBikeFinder2
+//
+//  Created by Michael Aicher on 16.01.15.
+//  Copyright (c) 2015 foryouandyourcustomers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
